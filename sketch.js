35acd696bcaf87741ee20826bce4ab6ca947ba82
Tanishka002 <75033184+Tanishka002@users.noreply.github.com>
2021-04-08 14:13:58 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var hr = hour();
+var mn = minute();
+var sc = second();
+
+function setup() {
+  createCanvas(800,800);
+  angleMode(DEGREES);
+
+}
+
+function draw() {
+  background(255,255,255); 
+  translate(300,300);
+  rotate(-100);
+
+  hr=hour();
+  mn=minute();
+  sc=second();
+
+  scAngle=map(sc,0,60,0,360);
+  mnAngle=map(mn,0,60,0,360);
+  hrAngle=map(hr%12,0,12,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke("red");
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke("green");
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke("blue");
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+
+ 
+  strokeWeight(10);
+  noFill();
+  
+  stroke("red");
+  arc(0,0,300,300,0,scAngle);
+
+  stroke("green");
+  arc(0,0,280,280,0,mnAngle);
+
+  stroke("blue");
+  arc(0,0,260,260,0,hrAngle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  drawSprites();
+}
